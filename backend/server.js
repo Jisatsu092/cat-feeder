f236e8a0338f961036782bb16de51b5516fe95d5
Jisatsu092 <rizkyalikhsan2005.com>
2026-04-04 08:15:04 +0700

    fix: url

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const scheduleRoutes = require('./src/routes/schedule');
@@ -7,12 +8,25 @@ const { loadSchedules } = require('./src/services/scheduler');
 const { initDb } = require('./src/models/db');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+let tunnelUrl = '';
+
+app.get('/api/tunnel-url', (req, res) => {
+  res.json({ url: tunnelUrl });
+});
+
+app.post('/api/tunnel-url', (req, res) => {
+  tunnelUrl = req.body.url;
+  console.log('[TUNNEL] URL updated:', tunnelUrl);
+  res.json({ ok: true });
+});
 
 app.use('/api/schedules', scheduleRoutes);
 app.use('/api/feed', feedRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Cat Feeder API Running');
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 initDb().then(() => {
